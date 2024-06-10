624eee7645891adb2f25cb37feddc2665c87cb42
Fernando Mata <matarabadanf@gmail.com>
2024-06-11 01:15:40 +0200

    Alberto

--- sub/alberto.py
+++ sub/alberto.py
@@ -29,8 +29,48 @@ file = open(args.f, 'r')
 cont = file.readlines()
 
 # grep the lines of interest 
+counter = 0 
+n_os = []
+for line in cont:
+    if 'TD-DFT/TDA EXCITED STATES (SINGLETS)' in line:
+        counter+=1
+        if counter >= 2:
+            break
+    elif 'STATE' in line and 'TD-DFT/TDA EXCITED STATES (SINGLETS)' not in line and 'EXCITED STATE GRADIENT DONE' not in line:
+        n_os.append(int(line.strip().split()[1].replace(':', '')))
 
+total_list = [[] for i in range(0, max(n_os))]
+
+print(total_list)
+
+cis_energies = []
+ 
 for line in cont:
     if 'STATE' in line and 'TD-DFT/TDA EXCITED STATES (SINGLETS)' not in line and 'EXCITED STATE GRADIENT DONE' not in line:
-        print(line)
+        index = int(line.strip().split()[1].replace(':', '')) - 1
+        energy = float(line.strip().split()[3].replace(':', ''))
+        total_list[index].append(energy)
+    elif 'E(SCF)' in line:
+        cis_energies.append(float(line.strip().split()[2]))
 
+print(total_list)
+
+cis_array = np.array(cis_energies)
+total_arrays = [np.array(l) for l in total_list]
+
+
+x = np.arange(1, len(total_list[0])+1, 1)
+
+print(x)
+
+plt.plot(x, cis_array, label='Ground state')
+
+for i in range(len(total_arrays)):
+    plt.plot(x, cis_array+total_arrays[i], label='Root %i'%(i+1))
+
+plt.legend()
+
+plt.xlabel('Step')
+plt.ylabel('Energy / Hartree')
+
+plt.show()
