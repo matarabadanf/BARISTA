2d842fdae878a2d6b61fa8dda269f73fdd9b2f2c
Fernando Mata <matarabadanf@gmail.com>
2024-12-29 21:34:05 +0100

    Refactor of Laura and bug fix. Now generates appropriate xyz files with engrad calculations.

--- personnel/laura.py
+++ personnel/laura.py
@@ -15,10 +15,142 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "-f", type=str, required=True, default=42, help="Input file name"
 )
-parser.add_argument("-o", type=str, default=True, help="Output file name")
-parser.add_argument(
-    "-g", type=str, default="True", help="Generate an xyz file"
-)
+parser.add_argument("-o", type=str, default=None, help="Output file name")
+
+parser.add_argument("--no_energies", default=True, action='store_false', help="Dont include energies in the xyzfile as comment.")
+
+
+class Laura:
+
+    def __init__(self, orca_output_filename):
+        self.orca_output_filename = orca_output_filename
+        self._file_content = None
+        self._gradient_calculation = False
+        self.ground_state_energy = None
+        self.energies = None
+        self._coordinates_dataframe = None
+
+        self._initialize()
+
+    @classmethod
+    def from_file(cls, orca_output_filename):
+        instance = cls(orca_output_filename)
+
+        return instance
+
+    def _initialize(self):
+        self._get_file_content()
+        self._get_nroots()
+        self._is_gradient_calculation()
+        self._get_gs_energy()
+        self._get_excitation_energies()
+        self._get_coordinates_section()
+        self._extract_coordinates_to_dataframe()
+
+    def _get_file_content(self):
+        try:
+            with open(self.orca_output_filename, "r") as f:
+                self._file_content = f.readlines()
+
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Could not find file: {self.orca_output_filename}")
+
+    def _get_nroots(self):
+        for line in self._file_content:
+            if "Number of roots to be determined" in line:
+                self.n_roots = int(line.strip().split()[-1])
+                break
+
+    def _is_gradient_calculation(self):
+        for line in self._file_content:
+            if 'SCF GRADIENT' in line:
+                self._gradient_calculation = True
+                break
+
+    def _get_gs_energy(self):
+
+        if self._gradient_calculation:
+            for line in self._file_content:
+                if 'Total Energy       : ' in line:
+                    self.ground_state_energy = float(line.strip().split()[-4])
+
+        else:
+            for line in self._file_content:
+                if "E(SCF)  =" in line:
+                    print(line)
+                    self.ground_state_energy = float(line.strip().split()[-2])
+
+    def _get_excitation_energies(self):
+        self.energies = np.zeros(self.n_roots + 1)
+        self.energies[0] = self.ground_state_energy
+
+        for line in self._file_content:
+            if (
+                "STATE" in line
+                and "TD-DFT/TDA EXCITED STATES (SINGLETS)" not in line
+                and "EXCITED STATE GRADIENT DONE" not in line
+                and "NATURAL" not in line
+            ):
+
+                index = int(line.replace(':', ' ').strip().split()[1])
+                self.energies[index] = float(line.strip().split()[3]) + self.ground_state_energy
+
+    def _get_coordinates_section(self):
+
+        for index, line in enumerate(self._file_content):
+            if "CARTESIAN COORDINATES (ANGSTROEM)" in line:
+                self.starting_index = index + 2
+            elif "CARTESIAN COORDINATES (A.U.)" in line:
+                self.end_index = index - 2
+
+        return self.starting_index, self.end_index
+
+    def _extract_coordinates_to_dataframe(self):
+
+        positions = np.array(
+            [
+                np.array(vector.strip().split()[1:]).astype(float)
+                for vector in self._file_content[self.starting_index:self.end_index]
+            ]
+        )
+
+        labels = [
+            np.array(vector.strip().split()[0])
+            for vector in self._file_content[self.starting_index:self.end_index]
+        ]
+
+        df = pd.DataFrame(
+            {
+                "Atom labels": labels,
+                "x": positions[:, 0],
+                "y": positions[:, 1],
+                "z": positions[:, 2],
+            }
+        )
+
+        self._coordinates_dataframe = df
+
+    def get_coordinates_dataframe(self):
+
+        return self._coordinates_dataframe.copy()
+
+    def generate_xyzfile(self, filename=None, print_energies=True):
+        if filename is None:
+            filename = (
+                self.orca_output_filename.replace(".in", "").replace(".out", "") + ".xyz"
+            )
+
+        with open(filename, "w") as out_file:
+            out_file.write(str(len(self._file_content[self.starting_index:self.end_index])) + "\n")
+
+            if print_energies:
+                out_file.write(str(self.energies).replace(']', '').replace('[', '') + "\n")
+
+            else:
+                out_file.write('Imagine a world where atoms were very small kittens\n')
+
+            for i in self._file_content[self.starting_index:self.end_index]:
+                out_file.write(i.strip() + "\n")
 
 
 def laura(
@@ -32,27 +164,25 @@ def laura(
     for line in cont:
         if "Number of roots to be determined" in line:
             n_roots = int(line.strip().split()[-1])
-        elif 'ORCA SCF GRADIENT CALCULATION' in line:
+        elif 'GRADIENT' in line:
             engrad = True
 
     gs_energy = 0
 
     engrad = False
 
-    if engrad == True:
-        for line in cont:
-            if 'Total Energy ' in line:
-                gs_energy = float(line.strip().split()[-4])
+    if 'Total Energy ' in line:
+        gs_energy = float(line.strip().split()[-4])
 
     for line in cont:
-         if "E(SCF)  =" in line:
-             gs_energy = float(line.strip().split()[-2])
-             print('ground state energy is', gs_energy)
+        if "E(SCF)  =" in line:
+            gs_energy = float(line.strip().split()[-2])
+            print('ground state energy is', gs_energy)
 
     energies = np.zeros(n_roots + 1)
     energies[0] = gs_energy
 
-    counter = 0 
+    counter = 0
 
     for line in cont:
         if (
@@ -61,7 +191,7 @@ def laura(
             and "EXCITED STATE GRADIENT DONE" not in line
             and "NATURAL" not in line
         ):
-            index = int(line.replace(':',' ').strip().split()[1])
+            index = int(line.replace(':', ' ').strip().split()[1])
             energies[index] = float(line.strip().split()[3])
 
     energies[1:] += gs_energy
@@ -125,6 +255,6 @@ if __name__ == "__main__":
         sys.exit(1)
 
     args = parser.parse_args()
-    print(args.g)
-    a = laura(args.f, args.o, args.g)
-    print(a[1])
+
+    a = Laura.from_file(args.f)
+    a.generate_xyzfile(args.o, args.no_energies)
