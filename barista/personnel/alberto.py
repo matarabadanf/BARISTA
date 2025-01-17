ef7f723dc4d95623d3c8361af6cb2306446aae5e
Fernando Mata <matarabadanf@gmail.com>
2025-01-17 16:51:01 +0100

    Some alberto progress

--- barista/personnel/alberto.py
+++ barista/personnel/alberto.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 
-# Parser is used to input via terminal the required arguments for Emma
+# Parser is used to input via terminal the required arguments
 parser = argparse.ArgumentParser(
     description="""Takes a excited state optimization in ORCA and plots the energy of each state at each step of the opitmizaiton and the actual root.""",
     epilog="""It should run appropriately with ORCA 5.0.X""",
@@ -23,6 +23,193 @@ parser.add_argument(
 parser.add_argument("-u", type=str, default="eV", help="Energy units")
 
 # display help message if there is no arguments
+
+
+class Alberto:
+    """
+    Takes a excited state optimization in ORCA and plots the energy of each state at each
+    step of the opitmizaiton and the actual root.
+
+    Attributes
+    ----------
+    filename : str
+        Name of the orca output file.
+    reference_energy : float
+        Value of the reference energy.
+    units : str
+        Units for the plot. Default is eV.
+
+
+    Methods
+    -------
+    plot()
+        Plots the energies at each step of the optimization.
+
+    save_image(image_name)
+        Saves the plot to an image.
+    """
+
+    def __init__(
+        self,
+        filename: str,
+        reference_energy: float = 0.0,
+        units: str = "eV",
+    ) -> None:
+        self.filename = filename
+        self.reference_energy = reference_energy
+        self.units = units
+        self.content_list = []
+
+        self._initialize()
+
+    def _initialize(self):
+        self._get_file_content()
+        self._get_state_indexes()
+        self._get_cis_energies()
+        self._get_energy_array()
+        self._rescale_units(self.units)
+
+    def _get_file_content(self):
+        """
+        Open the file and store its content as a list
+
+        Raises
+        ------
+        ValueError
+            If there is no filename specified.
+        FileNotFoundError
+            If the file does not exist.
+
+        Returns
+        -------
+        None.
+
+        """
+        if self.filename is None:
+            raise ValueError("No result file was specified")
+
+        try:
+            with open(self.filename, "r") as file:
+                self.content_list = file.readlines()
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File {self.filename:s} does not exist.")
+
+    def _get_state_indexes(self):
+        if self.filename is None:
+            raise ValueError("No result file was found")
+
+        counter = 0
+        self.state_indexes = []
+
+        for line in self.content_list:
+            if "TD-DFT/TDA EXCITED STATES (SINGLETS)" in line:
+                counter += 1
+                if counter >= 2:
+                    break
+            elif (
+                "STATE" in line
+                and "TD-DFT/TDA EXCITED STATES (SINGLETS)" not in line
+                and "EXCITED STATE GRADIENT DONE" not in line
+            ):
+                self.state_indexes.append(
+                    int(line.strip().split()[1].replace(":", ""))
+                )
+
+        self.number_of_states = len(self.state_indexes)
+
+    def _get_cis_energies(self):
+        cis_energies = []
+
+        for line in self.content_list:
+            if "E(SCF)" in line:
+                cis_energies.append(float(line.strip().split()[2]))
+
+        if self.reference_energy == 0:
+            self.reference_energy = cis_energies[0]
+
+        self.cis_array = (
+            np.array(cis_energies).reshape(-1) - self.reference_energy
+        )
+
+        self.number_of_steps = len(self.cis_array)
+
+    def _get_energy_array(self):
+        total_energy_list = [[] for i in range(self.number_of_states)]
+
+        for line in self.content_list:
+            if (
+                "STATE" in line
+                and "TD-DFT/TDA EXCITED STATES (SINGLETS)" not in line
+                and "EXCITED STATE GRADIENT DONE" not in line
+            ):
+                index = int(line.strip().split()[1].replace(":", "")) - 1
+                energy = float(line.strip().split()[3].replace(":", ""))
+                total_energy_list[index].append(energy)
+
+        self.energy_array = np.zeros(
+            [self.number_of_states, self.number_of_steps]
+        )
+
+        for pes_index, state in enumerate(total_energy_list):
+            for index, energy in enumerate(state):
+                self.energy_array[pes_index, index] = energy
+
+    def set_units(self, units):
+        prev_units = self.units
+
+        if units in ["eV", "Hartree"]:
+            self.units = units
+        else:
+            raise ValueError('Units can only be "eV" or "Hartree".')
+
+        self._rescale_units(prev_units)
+
+    def _rescale_units(self, prev_units):  # ??? Something is wrong, redo
+        if (
+            self.units == prev_units
+            and self.units == "eV"
+            or self.units != prev_units
+            and self.units == "eV"
+        ):
+            self.cis_array *= 27.2114
+            self.energy_array *= 27.2114
+
+        elif (
+            self.units == prev_units
+            and self.units == "Hartree"
+            or self.units != prev_units
+            and self.units == "Hartree"
+        ):
+            self.cis_array /= 27.2114
+            self.energy_array /= 27.2114
+
+    def set_imagename(self, image_name: str):
+        """
+        Sets the class parameter Alberto.imagename
+
+        Parameters
+        ----------
+        image_name : str
+            Image name to set.
+
+        Returns
+        -------
+        None,
+
+        """
+        self.output_image = image_name
+
+    def plot(self):
+        x_range = np.arange(0, self.number_of_steps, 1)
+        for state in self.energy_array:
+            plt.plot(x_range, state)
+        plt.plot(x_range, self.cis_array)
+
+    def generate_image(self, output_image_name: str):
+
+        self.plot()
+
+        plt.savefig(output_image_name, dpi=300)
 
 
 def alberto(
@@ -124,10 +311,10 @@ def alberto(
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        parser.print_help(sys.stderr)
-        sys.exit(1)
+    # if len(sys.argv) == 1:
+    #     parser.print_help(sys.stderr)
+    #     sys.exit(1)
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
-    a = alberto(args.f, args.en, args.o, args.u)
+    a = Alberto("upgrades/beta-carboline_opt_followiroot_3.in.out")
