0d495f03bb0209b95689ad91bda4a63744d2e40d
Fernando Mata <matarabadanf@gmail.com>
2025-02-28 17:01:19 +0100

    Javi fixes

--- barista/personnel/javi.py
+++ barista/personnel/javi.py
@@ -4,7 +4,9 @@ import argparse
 import os
 from typing import Tuple
 import numpy as np
-
+import plotly.graph_objects as go
+import sys
+import matplotlib.pyplot as plt 
 
 # Parser is used to input via terminal the required arguments for Emma
 parser = argparse.ArgumentParser(
@@ -14,48 +16,35 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument(
-    "-fc",
+    "-g0",
     type=str,
     required=True,
-    default=42,
-    help="Optimized ground state .xyz file",
+    default='',
+    help="Lower state gradient file.",
 )
+
 parser.add_argument(
-    "-ex",
+    "-g1",
     type=str,
     required=True,
-    nargs="+",
-    default=42,
-    help='Excited state optimization .out "filenames" (it is important that the name ends with *_N.in.out)',
+    default='',
+    help="Higher state gradient file.",
 )
+
 parser.add_argument(
-    "-O",
+    "-nac",
     type=str,
-    default=42,
-    help='Output file extension (default=".in.out")',
-)
-
-parser.add_argument("-o", type=str, default=42, help="Output image filename")
-
-parser.add_argument(
-    "-r", type=str, default=42, help="Generate a report table in output file"
+    required=True,
+    default='',
+    help="NAC vector file.",
 )
 
 parser.add_argument(
-    "-md",
-    type=str,
-    default=True,
-    help="Generate report in MD format (default is True)",
+    "--interactive",
+    action='store_true',
+    required=False,
+    help="Open interactive plot.",
 )
-parser.add_argument(
-    "-i",
-    type=bool,
-    default=False,
-    help="Interactive plot mode (default is False)",
-)
-
-# display help message if there is no arguments
-
 
 class Javi:
 
@@ -76,6 +65,12 @@ class Javi:
     def _init(self) -> None:
         self._load_vectors()
 
+   
+    @classmethod
+    def from_xy(cls):
+        pass 
+
+
     @property
     def ci_energy(self) -> float:
         return float(self._ci_energy)
@@ -87,7 +82,7 @@ class Javi:
 
     @cached_property
     def g_ab(self) -> np.ndarray:
-        return 0.5 * np.copy(self._ga + self._gb)
+        return 0.5 * np.copy(self._gb - self._ga)
 
 
     @cached_property
@@ -100,17 +95,19 @@ class Javi:
 
     @cached_property
     def beta(self) -> float:
-        tan_2_beta = 2 * (np.dot(self.g_ab, self.h_ab)) / (np.linalg.norm(self.g_ab) - np.linalg.norm(self.h_ab))
+        # tan_2_beta = 2 * (np.dot(self.g_ab, self.h_ab)) / (np.linalg.norm(self.g_ab) - np.linalg.norm(self.h_ab))
+        # return np.arctan(tan_2_beta)
+        tan_2_beta = 2 * (np.dot(self.g_ab, self.h_ab)) / (np.dot(self.g_ab, self.g_ab) - np.dot(self.h_ab, self.h_ab))
 
-        return np.atan(tan_2_beta)
+        return 0.5 * np.arctan2(2 * np.dot(self.g_ab, self.h_ab), (np.dot(self.g_ab, self.g_ab) - np.dot(self.h_ab, self.h_ab)))
 
     @cached_property
     def _g_tilde(self) -> np.ndarray:
-        return self.g_ab * np.cos(self.beta) + self.h_ab - np.sin(self.beta)
+        return self.g_ab * np.cos(self.beta) + self.h_ab * np.sin(self.beta)
 
     @cached_property
     def _h_tilde(self) -> np.ndarray:
-        return self.g_ab * np.cos(self.beta) - self.h_ab - np.sin(self.beta)
+        return self.h_ab * np.cos(self.beta) - self.g_ab * np.sin(self.beta)
 
     @cached_property
     def x(self) -> np.ndarray:
@@ -124,13 +121,13 @@ class Javi:
     def pitch(self) -> float:
         ''' Pitch \\delta_gh. '''
 
-        return np.sqrt( 1/2 * (np.linalg.norm(self._g_tilde) + np.linalg.norm(self._h_tilde)))
+        return np.sqrt( 1/2 * (np.dot(self._g_tilde, self._g_tilde) + np.dot(self._h_tilde, self._h_tilde)))
 
     @cached_property
     def asymmetry(self) -> float:
         ''' Asymmetry \\Delta_gh. '''
 
-        asym = (np.linalg.norm(self._g_tilde) - np.linalg.norm(self._h_tilde)) / (np.linalg.norm(self._g_tilde) + np.linalg.norm(self._h_tilde))
+        asym = (np.dot(self._g_tilde, self._g_tilde) - np.dot(self._h_tilde, self._h_tilde)) / (np.dot(self._g_tilde, self._g_tilde) + np.dot(self._h_tilde, self._h_tilde))
 
         return float(asym)
 
@@ -138,6 +135,7 @@ class Javi:
         return 2 * self.pitch * np.sqrt((x**2 + y**2) + self.asymmetry * (x**2 - y**2))
  
     def average_energy(self, x:float, y:float) -> float:
+        # return x + y  
         return self.ci_energy + x * np.dot(self.s_ab, self.x) + y * np.dot(self.s_ab, self.y)
 
     def E_A(self, x:float, y:float) -> float:
@@ -151,24 +149,39 @@ class Javi:
         return aver + diff
 
     @cached_property
-    def theta_s(self, max_grid = .1):
-        x = np.linspace(-max_grid, max_grid, 101)
-        y = np.linspace(-max_grid, max_grid, 101)
-        
-        X, Y = np.meshgrid(x, y)
-        b_p = self.ci_energy + np.zeros_like(X)
-        e_mean = self.average_energy(X, Y)
-    
-    
-        max_index = np.unravel_index(np.argmax(e_mean), e_mean.shape)
-        
-        max_x = X[max_index]
-        max_y = Y[max_index]
+    def theta_s(self, n_points:int = 360, radius:float=1):
 
-        vec = np.array([max_x, max_y])
-        x = np.array([1,0])
+        angles = np.linspace(0, 2 * np.pi, n_points, endpoint=False)
+        x_points = radius * np.cos(angles)
+        y_points = radius * np.sin(angles)
 
-        theta = np.arccos(np.dot(vec, x) / (np.linalg.norm(vec) * np.linalg.norm(x)))
+        pairs = [[x_points[i], y_points[i]] for i in range(n_points)]
+
+        samples = []
+
+        for pair in pairs:
+            x, y = pair
+            sample = [x, y, self.average_energy(x, y)]
+            print(f'\n\n\n The xyz values of tilt is: {x:5.2f}, {y:5.2f}, {self.average_energy(x,y):5.2f}')
+            samples.append(sample)
+
+        x,y,z = max(samples, key=lambda item: item[2])
+        print(f'\n\n\n The xyz values of the max tilt is: {x:5.2f}, {y:5.2f}, {z:5.2f}')
+
+        vec = np.array([x,y])
+
+        print(vec)
+        cos_theta = np.dot(vec, np.array([1,0])) / np.linalg.norm(vec)
+        cos_theta = np.clip(cos_theta, -1.0, 1.0)
+
+        theta = np.arccos(cos_theta)
+ 
+#        theta = -theta if vec[0] < 0 else theta
+
+        print(f'The angle theta is {theta} or {(theta+np.pi/2)%np.pi}')
+        
+
+        print(f'The angle of the maximum tilt is {theta:5.3} Radians or {theta*360/np.pi:3.5} degrees')
         
         return theta
 
@@ -197,14 +210,10 @@ class Javi:
 
         return (b, b_type)
 
-    def plot_CI(self, max_grid:float=0.1):
-        try:
-            import plotly.graph_objects as go
-        except:
-            print('Plotly is required')
+    def plot_CI(self, max_grid:float=1):
     
-        x = np.linspace(-max_grid, max_grid, 101)
-        y = np.linspace(-max_grid, max_grid, 101)
+        x = np.linspace(-max_grid, max_grid, 501)
+        y = np.linspace(-max_grid, max_grid, 501)
         
         X, Y = np.meshgrid(x, y)
         e_a = self.E_A(X, Y)
@@ -221,16 +230,15 @@ class Javi:
         y_intersect = Y[intersection_mask]
         z_intersect = b_p[intersection_mask] 
     
-        max_index = np.unravel_index(np.argmax(e_mean), e_mean.shape)
+        # Floor for contour 
+        z_min = min(e_a.flatten()) - (max(e_a.flatten()) - min(e_a.flatten())) * 0.05  # Slightly below the minimum
+
+        # Create a constant z-plane at the bottom to display the contour
+        z_floor = np.ones(e_a.shape) * z_min
         
-        # Find the x, y values corresponding to the maximum
-        max_x = X[max_index]
-        max_y = Y[max_index]
-        
-        # Find the maximum value
-        max_value = e_mean[max_index]
-        
-        print(f"The maximum value is {max_value} at x = {max_x}, y = {max_y}")
+        max_x = max_grid * np.cos(self.theta_s)
+        max_y = max_grid * np.sin(self.theta_s)
+        max_value = self.average_energy(max_x,max_y)
     
         # rgba(100, 120, 140, 0.7)
         # Create a 3D surface plot
@@ -239,11 +247,11 @@ class Javi:
             go.Surface(z=e_b,    x=X, y=Y, showscale=False, name='Surface 2', opacity=0.8, colorscale=[[0, 'rgba(173, 216, 230, 0.7)'],[1, 'rgba(173, 216, 230, 0.7)']]),
             go.Surface(z=e_mean, x=X, y=Y, showscale=False, name='Branching plane', opacity=0.5, colorscale=[[0, 'rgba(255, 0, 0, 0.7)'],[1, 'rgba(255, 0, 0, 0.7)']]),
             go.Surface(z=b_p,    x=X, y=Y, showscale=False, name='Mean energy plane', opacity=0.5, colorscale=[[0, 'rgba(0, 255, 0, 0.7)'],[1, 'rgba(0, 255, 0, 0.7)']]),
-    
+
             # Vectors
             go.Scatter3d(x=x_intersect, y=y_intersect, z=z_intersect, mode='lines+text', line=dict(color='rgba(0, 0, 0, 0.5)', width=4), name='BP and ME intersection'),
             go.Scatter3d(x=[0, max_x], y=[0, max_y], z=[0, max_value], mode='lines+text',line=dict(color='black', width=4), name = r'Max tilt direction', text=['',  'Max tilt direction'], textfont=dict(color='black')),
-            go.Scatter3d(x=[0, 1*max_grid], y=[0, 0], z=[0, 0], mode='lines+text',line=dict(color='black', width=4), name = r'$\hat{\mathbf{x}}$', text=['',  'x'], textfont=dict(color='black')),
+            go.Scatter3d(x=[0, 1*max_grid], y=[0, 0], z=[0, 0], mode='lines+text',line=dict(color='black', width=4), name = r'$\hat{\mathbf{x}}$', text=['', 'x'], textfont=dict(color='black')),
             go.Scatter3d(x=[0, 0], y=[0, 1*max_grid], z=[0, 0], mode='lines+text',line=dict(color='black', width=4), name = r'$\hat{\mathbf{y}}$', text=['', 'y'], textfont=dict(color='black')),
     
             # Dummy scatter traces for the legend to reflect the surface color
@@ -284,29 +292,33 @@ class Javi:
             )
         )
     
-        fig.show()
-
-    
+        fig.show()    
 
 if __name__ == "__main__":
-    # if len(sys.argv) == 1:
-    #     parser.print_help(sys.stderr)
-    #     sys.exit(1)
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
-    # args = parser.parse_args()
-
+    args = parser.parse_args()
     
-    # TESTING
     j = Javi(
-        'engrad_0_gradient.dat', 
-        'engrad_1_gradient.dat', 
-        'y_minus_one.dat',
+        args.g0,
+        args.g1,
+        args.nac
     )
 
-    print(j.g_ab)
-    print(j.beta)
-
-    j.plot_CI()
-
+    if args.interactive == True:
+        j.plot_CI()
+    
+    # TESTING
+    # j = Javi(
+    #     'engrad_0_gradient.dat', 
+    #     'engrad_1_gradient.dat', 
+    #     'y_minus_one.dat',
+    # )
+    # print(j.g_ab)
+    # print(j.beta)
+    # if args.interactive == True:
+    #     j.plot_CI()
 
 
